a8d83fc358bf544eca75eaa2ca02228e8be6bc08
wjacobw <williamjacob0910@gmail.com>
2023-10-23 23:53:47 +0800

    Add stats component

--- Bank_app/src/components/Business.jsx
+++ Bank_app/src/components/Business.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { layout } from "../style";
 
 const Business = () => {
   return (
-    <div>Business</div>
-  )
-}
+    <section id="features" className={layout.section}>
+      <div className = {layout.sectionInfo}>
+        <h2>You do the business, <br/>   We will handle the money</h2>
+        <p>With the right credit card, you can improve your 
+          financial life by building credit, earning rewards, and
+          saving money. But with hundreds of credit cards on the market.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default Business
+export default Business;
